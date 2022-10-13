5892a149e799fc5f47353cd7a41b31a6a2965a05
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-12 23:36:36 -0500

    create strategy Error Boundaries

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({ app }) {
     return (
     <div className='p-5 bg-green-500 text-white text-3x1 font-bold'>
-        Header Spinner
+        Header Spinner | { app.name }
     </div>
 )}
--- prod-detail-page/src/App.jsx
+++ prod-detail-page/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import SafeComponent from './SafeComponent';
 
 import "./index.scss";
 
@@ -9,7 +10,9 @@ import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
+    <SafeComponent>
+      <Header app={{ name: 'PDP' }}/>
+    </SafeComponent>
     <div className="my-10">Home PDP Content</div>
     <Footer />
   </div>
