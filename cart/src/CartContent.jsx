230c93a1702b8d4eb55caac8e0f8077cdb38b007
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-14 09:37:00 -0500

    using minicart component.

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Login from './Login';
+import MiniCart from "./MiniCart";
+import { jwt } from './cart';
 
 export default function CartContent() {
+    
     const [ token, setToken ] = useState("");
     
+    useEffect(()=>{
+        return jwt.subscribe((val)=> setToken(val ?? ""))
+    },[]);
+
     return (
         <div>
             <Login />
+            <MiniCart />
             JWT: { token }
             </div>
     )
