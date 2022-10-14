0aeee5e628616cd83a303078a7b0dbe0fe082ea4
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-14 12:15:48 -0500

    general adjutsments.

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import MiniCart from 'cart/MiniCart';
+import Login from 'cart/Login';
 
-export default function Header({ app }) {
+
+export default function Header() {
     return (
     <div className='p-5 bg-green-500 text-white text-3x1 font-bold'>
-        Header Spinner | { app.name }
+        <div className="flex">
+            <div className="flex-grow">Fidget Spinner World</div>
+            <div className='flex-end relative'>
+                <MiniCart />
+                <Login />
+            </div>
+        </div>
     </div>
 )}
